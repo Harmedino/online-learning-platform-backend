92997ef17fd77dd4f1e1010aa20384feca56e8af
Harmedino <adamsahmed191@gmail.com>
2024-01-12 03:24:07 +0100

    auth started

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const cors = require('cors');
 
 const app = express()
+
+app.use(
+    cors({
+        origin: 'http://localhost:4200',
+        methods: ["GET", "POST", "DELETE", "PATCH"],
+        Credential:true
+    })
+)
 
 const port = 4300
 app.listen(port, () => {
